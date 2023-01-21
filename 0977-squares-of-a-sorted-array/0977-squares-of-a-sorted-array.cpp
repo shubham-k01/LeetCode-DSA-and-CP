e59afe2cc9a1ef9337c28e6584fc010ce9f4753e
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2023-01-21 23:28:35 +0530

    Time: 33 ms (86.14%), Space: 25.9 MB (58.84%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        //   long lo=0,hi=nums.size()-1;
-        //   while(lo<=hi){
-        //       if(abs(nums[lo])>nums[hi]){
-        //           swap(nums[lo],nums[hi]);
-        //       }
-        //           hi--;
-        //   }
-        //   swap(nums[0],nums[1]);
-        //   for(int i=0;i<nums.size();i++){
-        //       nums[i] = nums[i]*nums[i];
-        //   }
-        //   return nums;
-        for(int i=0;i<nums.size();i++){
-            nums[i] = nums[i] * nums[i];
+        // for(int i = 0; i<nums.size();i++){
+        //     nums[i] = nums[i]*nums[i];
+        // }
+        // sort(nums.begin(),nums.end());
+        // return nums;
+        
+        int lo = 0,hi =nums.size()-1;
+        vector<int> ans(nums.size());
+        int k = nums.size()-1;
+        
+        while(lo<=hi){
+            int max = abs(nums[lo])>nums[hi]?nums[lo]:nums[hi];
+            if(max==nums[lo]){
+                lo++;
+            }
+            else{
+                hi--;
+            }
+            ans[k] = max*max;
+            k--;
         }
-        sort(nums.begin(),nums.end());
-        return nums;
+        return ans;
     }
 };

273609316ca1d7ae2dead67141b440818d3c2497
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2023-02-11 19:57:23 +0530

    Time: 119 ms (74.78%), Space: 63.8 MB (23.56%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -8,16 +8,31 @@ public:
 //         for(int i=1;i<nums.size();i++){
 //             a = (a ^ i);
 //         }
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+//         unordered_map<int,int> mp;
+//         for(int i=0;i<nums.size();i++){
+//             mp[nums[i]]++;
+//         }
+//         for(auto pr:mp){
+//             if(pr.second > 1){
+//                 return pr.first;
+//             }
+//         }
+//         return -1;
+        
+//     }
+        int pele=nums[0];
+        int n=nums.size();
+        vector <int> vec(n);
+        for (int i=0;i<n;i++){
+            vec[nums[i]]++;
         }
-        for(auto pr:mp){
-            if(pr.second > 1){
-                return pr.first;
+        for (int i=0;i<n;i++){
+            if(vec[i]>1){
+                pele=i;
+                break;
             }
         }
-        return -1;
-        
-    }
+        return pele;
+    
+};
 };

44ab0b9efd922c53c198e3b44c5f4f7b00c9af13
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2023-02-18 22:54:18 +0530

    Time: 3 ms (87.17%), Space: 9.5 MB (58.77%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     
-    bool BS(vector<int> arr,int s,int e,int target){
-        int mid ;
-        while(s<=e){
-            mid = s + (e-s)/2;
-            if(arr[mid]==target){
-                return true;
-            }
-            if(arr[mid]>target){
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
-            }
-        }    
-        return false;
-    }
+    // bool BS(vector<int> arr,int s,int e,int target){
+    //     int mid ;
+    //     while(s<=e){
+    //         mid = s + (e-s)/2;
+    //         if(arr[mid]==target){
+    //             return true;
+    //         }
+    //         if(arr[mid]>target){
+    //             e=mid-1;
+    //         }
+    //         else{
+    //             s=mid+1;
+    //         }
+    //     }    
+    //     return false;
+    // }
+    
+    // bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     for(int i=0;i<matrix.size();i++){
+    //         vector<int> temp = matrix[i];
+    //         if(temp[0]<=target && temp[temp.size()-1]>=target){
+    //             return BS(temp,0,temp.size()-1,target);
+    //         }
+    //     }
+    //     return false;
+    // }
     
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++){
-            vector<int> temp = matrix[i];
-            if(temp[0]<=target && temp[temp.size()-1]>=target){
-                return BS(temp,0,temp.size()-1,target);
+        int row = matrix.size();
+        int column = matrix[0].size();
+        
+        int lo= 0;
+        int hi = (row*column)-1;
+        int mid;
+        while(lo<=hi){
+            mid = lo + (hi-lo)/2;
+            if(matrix[mid/column][mid%column]==target){
+                return true;
+            }
+            if(matrix[mid/column][mid%column]>target){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
             }
         }
         return false;

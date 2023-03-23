9615cf9d4b73f053239e553a2fed6cf0e7f13e52
shubham-k01 <101422378+shubham-k01@users.noreply.github.com>
2023-03-23 15:40:54 +0530

    Time: 7 ms (28.81%), Space: 7.2 MB (8.41%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,16 +1,53 @@
 class Solution {
 public:
+    
+//     bool isSame(int* a,int* b){
+//         for(int i=0;i<26;i++){
+//             if(a[i]!=b[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     string removeOccurrences(string s, string part) {
+//         int wlen = part.length();
+//         int i=0;
+//         int countPart[26] = {0};
+//         int countS[26] = {0};
+//         while(i<wlen){
+//             countPart[part[i]-'a']++;
+//             i++;
+//         }
+        
+//         i=0;
+//         while(i<wlen){
+//             countS[s[i]-'a']++;
+//             i++;
+//         }
+//         if(isSame(countS,countPart)){
+//             s.erase(0,wlen);
+//         }
+//         i=0;
+//         while(i<s.length()){
+//             countS[s[i++]-'a']++;
+//             countS[s[i-wlen]-'a']--;
+//             if(isSame(countS,countPart)){
+//                 s.erase(i-wlen,wlen);
+//                 i=0;
+//             }
+//         }
+//         return s;
+//     }
+    void removePart(string &s,string part,int p){
+        if(p==string::npos)
+            return ;
+        s.erase(p,part.length());
+        int i = s.find(part);
+        removePart(s,part,i);
+    }
     string removeOccurrences(string s, string part) {
-        while(s.length()>0 && s.find(part)<s.length()){
-            size_t found = s.find(part);
-            if(found != string::npos){
-                s.erase(found,part.length());
-            }
-            else{
-                break;
-            }
-            
-        }
+        int i = s.find(part);
+        removePart(s,part,i);
         return s;
     }
 };

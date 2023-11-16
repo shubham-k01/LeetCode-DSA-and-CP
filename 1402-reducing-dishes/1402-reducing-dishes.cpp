69729f811a59059c5688a6b9ce25f7248f8bdc54
Shubham Kurunkar <101422378+shubham-k01@users.noreply.github.com>
2023-11-16 20:31:08 +0530

    Time: 18 ms (54.09%), Space: 8.8 MB (56.69%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -7,7 +7,7 @@ public:
             return 0;
         }
         
-        int include = satisfaction[index] * (time) + solve(satisfaction,n,index+1,time+1);
+        int include = satisfaction[index] * (time+1) + solve(satisfaction,n,index+1,time+1);
         int exclude = 0 + solve(satisfaction,n,index+1,time);
         
         return max(include,exclude);
@@ -21,7 +21,7 @@ public:
         if(dp[index][time] != -1)
             return dp[index][time];
         
-        int include = satisfaction[index] * (time) + solveMem(satisfaction,n,index+1,time+1,dp);
+        int include = satisfaction[index] * (time+1) + solveMem(satisfaction,n,index+1,time+1,dp);
         int exclude = 0 + solveMem(satisfaction,n,index+1,time,dp);
         
         dp[index][time] = max(include,exclude);
@@ -44,15 +44,38 @@ public:
         return dp[0][0];
     }
     
+    int solveSpOp(vector<int>& satisfaction,int n){
+        
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
+        
+        for(int currIndex = n-1; currIndex >=0 ; currIndex--){
+//             start currTime from currIndex because the currIndex will be the total time for that index
+            for(int currTime = currIndex; currTime >= 0; currTime--){
+                int include = satisfaction[currIndex] * (currTime+1) + next[currTime+1];
+                int exclude = 0 + next[currTime];
+
+                curr[currTime] = max(include,exclude);
+            }
+            next = curr;
+        }
+        
+        return next[0];
+    }
+    
+    
+    
     int maxSatisfaction(vector<int>& satisfaction) {
         
         int n = satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        // return solve(satisfaction,n,0,1);
+        // return solve(satisfaction,n,0,0);
         
         // vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        // return solveMem(satisfaction,n,0,1,dp);
+        // return solveMem(satisfaction,n,0,0,dp);
         
-        return solveTab(satisfaction,n);
+        // return solveTab(satisfaction,n);
+        
+        return solveSpOp(satisfaction,n);
     }
 };

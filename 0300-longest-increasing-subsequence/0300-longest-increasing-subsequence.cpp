07355e4f35fe7fbcc5bb60a142962b588c217203
Shubham Kurunkar <101422378+shubham-k01@users.noreply.github.com>
2023-11-17 19:54:13 +0530

    Time: 515 ms (26.32%), Space: 292.7 MB (12.70%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -6,7 +6,7 @@ public:
             return 0;
         }
         
-        int include;
+        int include = 0;
         if(prev == -1 || nums[curr] > nums[prev]){
             include = 1 + solve(nums,n,curr+1,curr);
         }
@@ -35,30 +35,35 @@ public:
         return dp[curr][prev+1];
     }
     
-    int topDown(vector<int>& nums,int curr,int prev,vector<vector<int>>& dp){
-         if(curr>=nums.size()){
-            return 0;
+    int solveTab(vector<int> &nums){
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = curr; prev>= -1; prev--){
+                int include = 0;
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    include = 1 + dp[curr+1][curr+1];
+                }
+
+                int exclude = dp[curr+1][prev+1];
+
+                dp[curr][prev+1] = max(include,exclude);
+            }
         }
-        if(dp[curr][prev+1]!=-1)
-            return dp[curr][prev+1];
-        
-        int include=0;
-        if(prev==-1||nums[curr]>nums[prev])
-            include=1+topDown(nums,curr+1,curr,dp);
-        
-        //exclude
-        int exclude=topDown(nums,curr+1,prev,dp);
-        dp[curr][prev+1]= max(include,exclude);
-        return dp[curr][prev+1];
+        return dp[0][0];
     }
+    
+    
     
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         // return solve(nums,n,0,-1);
         
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        return solveMem(nums,0,-1,dp);
+        // vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        // return solveMem(nums,0,-1,dp);
         
-        // return topDown(nums,0,-1,dp);
+        return solveTab(nums);
     }
 };

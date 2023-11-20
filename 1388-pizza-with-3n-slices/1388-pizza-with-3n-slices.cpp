38ca59815e7021a1ccdc0073f29ee95216715f27
Shubham Kurunkar <101422378+shubham-k01@users.noreply.github.com>
2023-11-20 20:07:39 +0530

    Time: 23 ms (34.87%), Space: 23.5 MB (13.12%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -27,6 +27,40 @@ public:
         return dp[n][index]  = max(case1,case2);
     }
     
+    
+    int solveTab(vector<int> &slices){
+       
+        int n = slices.size();
+        vector<vector<int>> dp1(n+2,vector<int> (n+2,0));
+        vector<vector<int>> dp2(n+2,vector<int> (n+2,0));
+        
+        for(int k = 1; k <= n/3; k++){
+            for(int index = n-2; index >=0; index--){
+                int case1 = slices[index] + dp1[k-1][index+2];
+                int case2 = 0 + dp1[k][index+1];
+                
+                dp1[k][index] = max(case1,case2);
+            }
+        }
+        
+        int one = dp1[n/3][0];
+        
+        for(int k = 1; k <= n/3; k++){
+            for(int index = n-1; index >=1; index--){
+                int case1 = slices[index] + dp2[k-1][index+2];
+                int case2 = 0 + dp2[k][index+1];
+                
+                dp2[k][index] = max(case1,case2);
+            }
+        }
+        
+        int two = dp2[n/3][1];
+        
+        return max(one,two);
+        
+        
+    }
+    
     int maxSizeSlices(vector<int>& slices) {
         
         int n = slices.size();
@@ -35,12 +69,14 @@ public:
         // int case2 = solve(slices,n-1,n/3,1);
         
         
-        vector<vector<int>> dp1(n+1,vector<int> (n+1,-1));
-        vector<vector<int>> dp2(n+1,vector<int> (n+1,-1));
+        // vector<vector<int>> dp1(n+1,vector<int> (n+1,-1));
+        // vector<vector<int>> dp2(n+1,vector<int> (n+1,-1));
         
         
-        int case1 = solveMem(slices,n-2,n/3,0,dp1);
-        int case2 = solveMem(slices,n-1,n/3,1,dp2);
-        return max(case1,case2);
+        // int case1 = solveMem(slices,n-2,n/3,0,dp1);
+        // int case2 = solveMem(slices,n-1,n/3,1,dp2);
+        // return max(case1,case2);
+        
+        return solveTab(slices);
     }
 };

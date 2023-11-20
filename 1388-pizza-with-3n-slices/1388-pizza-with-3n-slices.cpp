d1ead7aef0c8762e3db660952b0c77aca122a7a5
Shubham Kurunkar <101422378+shubham-k01@users.noreply.github.com>
2023-11-20 19:37:22 +0530

    Time: 25 ms (22.67%), Space: 23.4 MB (22.32%) - LeetHub

---
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    int solve(vector<int> &slices, int endIndex, int n, int index ){
+        if(n == 0 || index > endIndex){
+            return 0;
+        }
+        
+        int case1 = slices[index] + solve(slices,endIndex,n-1,index+2);
+        int case2 = 0 + solve(slices,endIndex,n,index+1);
+        
+        return max(case1,case2);
+    }
+    
+    int solveMem(vector<int> &slices, int endIndex, int n, int index, vector<vector<int>> &dp){
+        if(n == 0 || index > endIndex){
+            return 0;
+        }
+        
+        if(dp[n][index] != -1){
+            return dp[n][index];
+        }
+        
+        int case1 = slices[index] + solveMem(slices,endIndex,n-1,index+2,dp);
+        int case2 = 0 + solveMem(slices,endIndex,n,index+1,dp);
+        
+        return dp[n][index]  = max(case1,case2);
+    }
+    
+    int maxSizeSlices(vector<int>& slices) {
+        
+        int n = slices.size();
+        // n/3 because pizza is of 3n slices so we will get to eat n slices
+        // int case1 =  solve(slices,n-2,n/3,0);
+        // int case2 = solve(slices,n-1,n/3,1);
+        
+        
+        vector<vector<int>> dp1(n+1,vector<int> (n+1,-1));
+        vector<vector<int>> dp2(n+1,vector<int> (n+1,-1));
+        
+        
+        int case1 = solveMem(slices,n-2,n/3,0,dp1);
+        int case2 = solveMem(slices,n-1,n/3,1,dp2);
+        return max(case1,case2);
+    }
+};

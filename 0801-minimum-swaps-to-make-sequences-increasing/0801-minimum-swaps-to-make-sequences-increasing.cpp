8f2c7cf4f2e9b946a091a7ee555245b897158a63
Shubham Kurunkar <101422378+shubham-k01@users.noreply.github.com>
2023-12-09 23:45:00 +0530

    Time: 153 ms (69.81%), Space: 90.6 MB (88.75%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -89,6 +89,49 @@ public:
         return dp[1][0];
     }
     
+    int solveSpOp(vector<int> &nums1, vector<int> &nums2){
+        int n = nums1.size();
+        int swap = 0, noswap = 0, currswap = 0, currnoswap = 0;
+        
+        for(int index = n-1 ; index >= 1; index--){
+            for(int swapped = 1; swapped >= 0; swapped--){
+                int ans = INT_MAX;
+        
+                int prev1 = nums1[index-1];
+                int prev2 = nums2[index-1];
+
+                // swapping previous values if needed before comparing for the current index
+                if(swapped){
+                    // swap(prev1,prev2);
+                    int temp = prev1;
+                    prev1 = prev2;
+                    prev2 = temp;
+                }
+
+                // noswap
+                if(nums1[index] > prev1 && nums2[index] > prev2){
+                    ans = noswap;
+                }
+
+                if(nums1[index] > prev2 && nums2[index] > prev1){
+                    ans = min(ans, 1 + swap);
+                }
+                
+                // dp[index][swapped] = ans;
+                if(swapped){
+                    currswap = ans; 
+                }
+                else{
+                    currnoswap = ans;
+                }
+                
+            } 
+            swap = currswap;
+            noswap = currnoswap;
+        }
+        return min(swap,noswap);
+    }
+    
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         
         nums1.insert(nums1.begin(),-1);
@@ -101,7 +144,8 @@ public:
         // vector<vector<int>> dp(n+1,vector<int> (2,-1));
         // return solveMem(nums1,nums2,n,1,swapped,dp);
         
-        return solveTab(nums1,nums2);
+        // return solveTab(nums1,nums2);
+        return solveSpOp(nums1,nums2);
         
         
     }
